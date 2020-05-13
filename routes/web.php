7f229d755bd21a57a5bd79b6520151afdb5865c2
Olaf1102 <olafkramer2001@gmail.com>
2020-05-13 12:36:55 +0200

    een fout opgelost dat een user zijn gegevens niet kon aanpassen

--- routes/web.php
+++ routes/web.php
@@ -21,14 +21,6 @@ Route::get('/', function () {
 // all routes like register/login etc.
 Auth::routes(['verify' => false, 'reset' => false ]);
 
-// this routes could only be accessed by an admin or user
-Route::group(['middleware' => ['role:admin|user']], function() {
-    // an redirect route when you are loged in
-    Route::get('/home', 'HomeController@index')->name('home');
-    // all url's for the notes
-    Route::resource('/notes','NotesController')->except(['create', 'show', 'edit']);
-});
-
 // this routes could only be accessed by the admin
 Route::group(['middleware' => ['role:admin']], function() {
     // admin panel for the users route 
@@ -37,12 +29,15 @@ Route::group(['middleware' => ['role:admin']], function() {
     Auth::routes(['register' => true, 'verify' => false, 'reset' => false ]);
 });
 
-// this routes could only be accessed by an user
-Route::group(['middleware' => ['role:user']], function() {
+// this routes could only be accessed by an admin or user
+Route::group(['middleware' => ['role:admin|user']], function() {
+    // an redirect route when you are loged in
+    Route::get('/home', 'HomeController@index')->name('home');
+    // all url's for the notes
+    Route::resource('/notes','NotesController')->except(['create', 'show', 'edit']);
     // route so you can change your own user data
-    Route::resource('/users','UsersController')->except(['update']);
+    Route::resource('/users','UsersController')->only(['update']);
 });
-
 
 // viewers of the site can't register
 Auth::routes(['register' => false, 'verify' => false, 'reset' => false ]);
